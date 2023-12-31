4c93c26cbe443947cd0e62a2776d64fa584539be
Iulia- Elena Pop <125976840+Iulia2191@users.noreply.github.com>
2023-12-31 18:54:30 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-10 text-center">
+      <div className="container mx-auto text-center">
         <p>&copy; Disney and Disney characters are trademarks of Walt Disney Company</p>
       </div>
     </footer>
