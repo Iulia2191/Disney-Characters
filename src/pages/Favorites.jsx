44853e1dd0bef7907353f04dd9636779da24b241
Iulia- Elena Pop <125976840+Iulia2191@users.noreply.github.com>
2023-12-31 18:53:43 +0100

    Update Favorites.jsx

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -41,7 +41,7 @@ const Favorites = () => {
       {favoriteCharacters.length > 0 && (
         <button
           onClick={() => dispatch(resetFavorite())}
-          className='bg-button text-white px-4 py-2 rounded mt-4 hover:bg-btnHover transition-colors cursor-pointer'
+          className='bg-button text-white px-4 py-2 rounded my-4 hover:bg-btnHover transition-colors cursor-pointer'
         >
           Reset Favorites
         </button>
